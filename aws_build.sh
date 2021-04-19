a344114f9907229afbbad7b27c7c726a22752d23
Qamar Abbas <syedqamar.a1@gmail.com>
2021-04-19 07:00:08 +0500

    Fixes

--- aws_build.sh
+++ aws_build.sh
@@ -25,5 +25,5 @@ cd ..
 rm -Rf $BUILD_ID
 FINAL_URL="https://${BUCKET_NAME}.s3-${REGION}.amazonaws.com/${PREFIX}${IPA_NAME}"
 
-$RESPONSE=curl --request POST \ --url $BASE_URL/api/build_manifest \ --header "Authorization: Bearer ${TOKEN}" \ --header "Content-Type: application/json" \ --header "Postman-Token: 11f4f673-479f-47b9-a28e-acc5f6ec03b1" \ --header "cache-control: no-cache" \ --data "{    \"url\": \"${FINAL_URL}\",\n    \"version\": \"${BUNDLE_ID}\",\n    \"id\": \"${BUILD_ID}\",\n    \"title\": \"${TITLE}\"}"
+$RESPONSE=$(curl --request POST \ --url $BASE_URL/api/build_manifest \ --header "Authorization: Bearer ${TOKEN}" \ --header "Content-Type: application/json" \ --header "Postman-Token: 11f4f673-479f-47b9-a28e-acc5f6ec03b1" \ --header "cache-control: no-cache" \ --data "{    \"url\": \"${FINAL_URL}\",\n    \"version\": \"${BUNDLE_ID}\",\n    \"id\": \"${BUILD_ID}\",\n    \"title\": \"${TITLE}\"}")
 echo "Api Response Returned\n ${RESPONSE}"
